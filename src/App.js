b73d45dabc5e98608f8158dbbe2b0d6aad33cc5d
Marc Cull <marc@anyroad.com>
2020-06-07 16:08:08 -0700

    hooking up some dummy logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,138 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const states = ['beenThere',
+                'open',
+                'blocked',
+                'actualBoom',
+                'potentialBoom',
+                'lastMove',
+                'monster',
+                'monster2',
+                'monster3'
+               ]
+const boardSize = 20;
+
+const randomState = () => {
+  return states[Math.floor(Math.random() * states.length)];
+}
+
+const getRandomBoard = () => {
+  let board = [];
+  for (let i = 0; i < boardSize; i++) {
+    board[i] = [];
+    for (let j = 0; j < boardSize; j++) {
+      board[i][j] = { state: randomState() }
+    }
+  }
+  return board;
+}
+
+const renderKey = () => {
+  return (
+    <div id="key" className="key">
+      &nbsp;
+      {
+        states.map((s) => {
+          return (
+            <div className="keyItemWithLabel">
+              <div className={`keyItem ${s}`}/>&nbsp;{s}
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+const renderBoard = (board, setBoard, updateScores, updateMessage) => {
+  return (
+    <div className="board">
+    { board.map((row, i) => {
+        return row.map((cell, j) => {
+          const type = board[i][j].state;
+          const key = "cell-" + i + "x" + j;
+          return (
+            <div key={key}
+                 className={`cell ${type}`}
+                 onClick={(e) => {
+                   console.log(`${i}x${j} was clicked`);
+                   console.log('pretending to send json back to server');
+                   const newBoard = getRandomBoard();
+                   console.log('pretending to get json back from server');
+                   console.log(newBoard);
+                   updateMessage(`${i}x${j}: ${type}`);
+                   updateScores(10);
+
+                   setBoard(newBoard);
+                 }}
+            />
+          )
+        })
+      })
+    }
+    </div>
+  )
+}
+
+const App = (props) => {
+  const [board, setBoard] = useState(() => {
+    return getRandomBoard();
+  });
+  const [score, setScore] = useState(0);
+  const [maxScore, setMaxScore] = useState(0);
+  const [totalScore, setTotalScore] = useState(0);
+  const [avgScore, setAvgScore] = useState(0);
+  const [numGames, setNumGames] = useState(0);
+  const [message, setMessage] = useState(null);
+
+  const updateScores = (points) => {
+    const newScore = score + points;
+    setScore(newScore);
+    setMaxScore(Math.max(maxScore,newScore));
+    setTotalScore(totalScore+points)
+  }
+
+  const updateMessage = (newMsg) => {
+    setMessage(newMsg);
+  }
+
+  const resetStats = () => {
+    setMaxScore(0);
+    setTotalScore(0);
+    setAvgScore(0);
+    setNumGames(0);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="left">
+        <div className="controls">
+          <div><button onClick={(e) => {
+            const newBoard = getRandomBoard();
+            setScore(0);
+            const totalGames = numGames + 1;
+            setNumGames(totalGames);
+            setAvgScore(totalScore/totalGames);
+            setBoard(newBoard);
+          }}>Start Over</button></div>
+          <div><button>Buy Safe Move</button> -50pts</div>
+          <div>Score: {score}</div>
+          {message && (<div>{message}</div>)}
+        </div>
+        { renderKey() }
+      </div>
+      { renderBoard(board, setBoard, updateScores, updateMessage) }
+      <div className="right">
+        <div className="controls">
+          <div>Num Skulls: 65</div>
+          <div>Num Bombs: 10</div>
+          <div>Possibilty of Bomb: 25%</div>
+          <div>Best Score: {maxScore}</div>
+          <div>Average Score: {Math.round(avgScore,2)}</div>
+          <div><button onClick={resetStats}>Reset stats</button></div>
+        </div>
+      </div>
     </div>
   );
 }
