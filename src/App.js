c78e182629c759c9f2a9e5f86bc45dc6b74fef5c
Marc Cull <marc@anyroad.com>
2020-06-08 02:09:02 -0700

    some formatting, and track preserving

--- src/App.js
+++ src/App.js
@@ -13,18 +13,55 @@ const states = ['beenThere',
                ]
 const boardSize = 20;
 
-const randomState = () => {
-  return states[Math.floor(Math.random() * states.length)];
+const shuffle = (array) => {
+  array.sort(() => Math.random() - 0.5);
+  return array;
 }
 
-const getRandomBoard = () => {
+const update = (board, row, col) => {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      const state = board[i][j].state;
+      if (i === row && j === col) {
+        board[i][j].state = 'lastMove';
+      } else if (state === 'lastMove') {
+        board[i][j].state = 'beenThere';
+      }
+    }
+  }
+  return board;
+}
+
+const getRandomCell = () => {
+  return  [Math.floor(Math.random()*boardSize),
+           Math.floor(Math.random()*boardSize)];
+}
+
+const init = (firstMove) => {
   let board = [];
   for (let i = 0; i < boardSize; i++) {
     board[i] = [];
     for (let j = 0; j < boardSize; j++) {
-      board[i][j] = { state: randomState() }
+      board[i][j] = {state: 'open' };
+      const edges = [0,boardSize-1];
+      if (edges.includes(i) && edges.includes(j)) {
+        board[i][j]['score'] = '+' + Math.floor(Math.random()*1000);
+      }
+      if (Math.random()*100 < 40) {
+        const boomType = Math.random()*100 < 50 ? 'actual' : 'potential';
+          board[i][j].state = `${boomType}Boom`;
+      } else if (Math.random()*100 < 20) {
+          board[i][j].state = 'blocked';
+      }
     }
   }
+  let [row,col] = firstMove;
+  board[row][col].state = 'lastMove';
+  const [row1,row2,row3] = shuffle([...Array(10).keys()].filter((n) => n != row)).slice(0,3);
+  const [col1,col2,col3] = shuffle([...Array(10).keys()].filter((n) => n != col)).slice(0,3);
+  board[row1][col1].state = 'monster';
+  board[row2][col3].state = 'monster2';
+  board[row3][col3].state = 'monster3';
   return board;
 }
 
@@ -45,28 +82,27 @@ const renderKey = () => {
   )
 }
 
-const renderBoard = (board, setBoard, updateScores, updateMessage) => {
+const renderBoard = (board, updateBoard, isValid, updateScores, updateMessage) => {
   return (
     <div className="board">
     { board.map((row, i) => {
         return row.map((cell, j) => {
           const type = board[i][j].state;
+          const score = board[i][j].score;
           const key = "cell-" + i + "x" + j;
+          const validSpace = isValid(i,j);
           return (
             <div key={key}
-                 className={`cell ${type}`}
+                 className={`cell ${type} c${i}-${j} ${validSpace ? 'neighbor' : ''}`}
                  onClick={(e) => {
-                   console.log(`${i}x${j} was clicked`);
-                   console.log('pretending to send json back to server');
-                   const newBoard = getRandomBoard();
-                   console.log('pretending to get json back from server');
-                   console.log(newBoard);
-                   updateMessage(`${i}x${j}: ${type}`);
-                   updateScores(10);
-
-                   setBoard(newBoard);
+                   if (validSpace) {
+                     const newBoard = update(board, i, j);
+                     updateMessage(`${i}x${j}: ${type}`);
+                     updateScores(10);
+                     updateBoard(newBoard, [i,j]);
+                   }
                  }}
-            />
+            ><div className="bonusScore">{score}</div></div>
           )
         })
       })
@@ -76,15 +112,17 @@ const renderBoard = (board, setBoard, updateScores, updateMessage) => {
 }
 
 const App = (props) => {
-  const [board, setBoard] = useState(() => {
-    return getRandomBoard();
-  });
+
   const [score, setScore] = useState(0);
   const [maxScore, setMaxScore] = useState(0);
   const [totalScore, setTotalScore] = useState(0);
   const [avgScore, setAvgScore] = useState(0);
   const [numGames, setNumGames] = useState(0);
   const [message, setMessage] = useState(null);
+  const [lastMove, setLastMove] = useState(() => getRandomCell());
+  const [board, setBoard] = useState(() => {
+    return init(lastMove);
+  });
 
   const updateScores = (points) => {
     const newScore = score + points;
@@ -104,24 +142,41 @@ const App = (props) => {
     setNumGames(0);
   }
 
+  const updateBoard = (board, lastMove) => {
+    setBoard(board);
+    setLastMove(lastMove);
+  }
+
+  const isValid = (row,col) => {
+    const x =  [row-1,row,row+1].includes(lastMove[0]) &&
+           [col-1,col,col+1].includes(lastMove[1]);
+    return x;
+  }
+
+  const handleStartOver = () => {
+    const firstMove = getRandomCell()
+    setLastMove(firstMove);
+    setBoard(init(firstMove));
+    setScore(0);
+    const totalGames = numGames + 1;
+    setNumGames(totalGames);
+    setAvgScore(totalScore/totalGames);
+
+  }
+
   return (
     <div className="App">
       <div className="left">
         <div className="controls">
-          <div><button onClick={(e) => {
-            const newBoard = getRandomBoard();
-            setScore(0);
-            const totalGames = numGames + 1;
-            setNumGames(totalGames);
-            setAvgScore(totalScore/totalGames);
-            setBoard(newBoard);
-          }}>Start Over</button></div>
+          <div>
+            <button onClick={()=>handleStartOver()}>Start Over</button>
+            </div>
           <div><button>Buy Safe Move</button> -50pts</div>
           <div>Score: {score}</div>
           {message && (<div>{message}</div>)}
         </div>
       </div>
-      { renderBoard(board, setBoard, updateScores, updateMessage) }
+      { renderBoard(board, updateBoard, isValid, updateScores, updateMessage) }
       <div className="right">
         <div className="controls">
           <div>Num Skulls: 65</div>
